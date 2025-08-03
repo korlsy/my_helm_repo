c12640af7864b58d5eb272a7e20bbb30a7b44c52
sangyeol.lee <korlsy@gmail.com>
2025-08-03 20:27:57 +0900

    auto_push_from_bash

--- auto_push.sh
+++ auto_push.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# helm package mynginx
+rm mynginx*.tgz
+helm package mynginx
+helm repo index .
 git add .;git commit -m "auto_push_from_bash";git push
 #helm repo list
 helm repo update
 #helm search repo mynginx
-rm -f /home/sangyeol/.cache/helm/repository/*.tgz
+#rm -f /home/sangyeol/.cache/helm/repository/*.tgz
